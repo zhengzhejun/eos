bf51c1089643d863b054b707f8d7cc9b38e26ab9
zhengzhejun <zhengzhejun@xiaomi.com>
2018-06-21 15:07:36 +0800

    fix

--- contracts/prochain/inlinetransfer/inlinetransfer.hpp
+++ contracts/prochain/inlinetransfer/inlinetransfer.hpp
@@ -1,7 +1,9 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp>
+
+
 struct trans_request {
     account_name from;
     account_name to;
-    asset quantity;
+    eosio::asset quantity;
 };
