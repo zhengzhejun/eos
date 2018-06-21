057236e9fb893e8c6bcd0e9159e99e74c6afcd83
zhengzhejun <zhengzhejun@xiaomi.com>
2018-06-21 12:04:39 +0800

    fix

--- contracts/prochain/addressbook/addressbook.cpp
+++ contracts/prochain/addressbook/addressbook.cpp
@@ -94,7 +94,7 @@ public:
         eosio::print("enter list\n");
         address_index addresses(_self, _self);
         auto phone_index = addresses.get_index<N(phone)>();
-        auto itr = phone_index.lower_bound(phone);
+        auto itr = phone_index.upper_bound(phone);
         for(; itr != phone_index.end(); itr++){
             eosio::print(itr->phone, "\n");
         }
