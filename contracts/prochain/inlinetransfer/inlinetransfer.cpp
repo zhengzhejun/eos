9c8cd5b5aaddec025326d6bca56157d6bea2ee8d
zhengzhejun <zhengzhejun@xiaomi.com>
2018-06-21 15:26:52 +0800

    fix

--- contracts/prochain/inlinetransfer/inlinetransfer.cpp
+++ contracts/prochain/inlinetransfer/inlinetransfer.cpp
@@ -19,13 +19,15 @@ public:
 
     //@abi action
     void transfer(const trans_request& request) {
+        require_auth(request.from);
+
         eosio_assert(request.quantity.is_valid(), "invalid quantity");
         eosio_assert(request.quantity.amount > 0, "must bigger than 0");
 
         eosio::print("enter transfer, ", eosio::name{request.from}, "\t", eosio::name{request.to}, "\t", request.quantity);
 
-        action(permission_level{request.from, N(active)}, N(eosio.token), N(transfer), std::make_tuple(request.from, request.to, request.quantity, std::string(""))
-        ).send();
+//        action(permission_level{request.from, N(active)}, N(eosio.token), N(transfer), std::make_tuple(request.from, request.to, request.quantity, std::string(""))
+//        ).send();
 
     }
 };
