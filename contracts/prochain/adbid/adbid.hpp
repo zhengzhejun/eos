43e92bea6cb45ae4be09eac7fe362c25dfe450e3
zhengzhejun <zhengzhejun@xiaomi.com>
2018-06-22 12:50:15 +0800

    fix

--- contracts/prochain/adbid/adbid.hpp
+++ contracts/prochain/adbid/adbid.hpp
@@ -27,8 +27,8 @@ struct PublishRequest {
     uint32_t bidstarttime;
     uint32_t bidendtime;
 
-    bool is_collide(eosio::time_point_sec stime, eosio::time_point_sec etime) {
+    bool is_collide(eosio::time_point_sec stime, eosio::time_point_sec etime) const {
         return !((starttime >= stime.utc_seconds && starttime < etime.utc_seconds) | (endtime > stime.utc_seconds && endtime <= etime.utc_seconds)
-                 | (stime.utc_seconds >= starttime && stime.utc_seconds < endtime) | (etime.utc_seconds > starttime && etime.utc_seconds <= endtime))
+                 | (stime.utc_seconds >= starttime && stime.utc_seconds < endtime) | (etime.utc_seconds > starttime && etime.utc_seconds <= endtime));
     }
 };
