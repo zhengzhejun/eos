8c5b99c1c352d053e78937c318268534b3e46d9a
zhengzhejun <zhengzhejun@chain.pro>
2018-12-28 00:24:03 +0800

    fix

--- programs/eosio-blocklog/main.cpp
+++ programs/eosio-blocklog/main.cpp
@@ -33,12 +33,15 @@ struct blocklog
       void initialize(const variables_map &options);
 
       bfs::path blocks_dir;
-      bfs::path output_file;
+      bfs::path eos_output_file;
+      bfs::path prochaintech_output_file;
       uint32_t first_block;
       uint32_t last_block;
       bool no_pretty_print;
       bool as_json_array;
+      abi_def eosio_abi_def;
       abi_def eosio_token_abi_def;
+      abi_def prochaintech_abi_def;
 };
 
 void blocklog::read_log()
@@ -73,33 +76,47 @@ void blocklog::read_log()
       //       }
       //    }
 
-      std::ofstream output_blocks;
-      std::ostream *out;
-      if (!output_file.empty())
+      std::ofstream eos_output_blocks;
+      std::ostream *eos_out;
+      if (!eos_output_file.empty())
       {
-            output_blocks.open(output_file.generic_string().c_str());
-            if (output_blocks.fail())
+            eos_output_blocks.open(eos_output_file.generic_string().c_str());
+            if (eos_output_blocks.fail())
             {
                   std::ostringstream ss;
-                  ss << "Unable to open file '" << output_file.string() << "'";
+                  ss << "Unable to open file '" << eos_output_file.string() << "'";
                   throw std::runtime_error(ss.str());
             }
-            out = &output_blocks;
+            eos_out = &eos_output_blocks;
       }
       else
-            out = &std::cout;
+            eos_out = &std::cout;
 
-      if (as_json_array)
-            *out << "[";
+      std::ofstream prochaintech_output_blocks;
+      std::ostream *prochaintech_out;
+      if (!prochaintech_output_file.empty())
+      {
+            prochaintech_output_blocks.open(prochaintech_output_file.generic_string().c_str());
+            if (prochaintech_output_blocks.fail())
+            {
+                  std::ostringstream ss;
+                  ss << "Unable to open file '" << prochaintech_output_file.string() << "'";
+                  throw std::runtime_error(ss.str());
+            }
+            prochaintech_out = &prochaintech_output_blocks;
+      }
+      else
+            prochaintech_out = &std::cout;
+
       uint32_t block_num = (first_block < 1) ? 1 : first_block;
       signed_block_ptr next;
       fc::variant pretty_output;
       const fc::microseconds deadline = fc::seconds(10);
       auto print_block = [&](signed_block_ptr &next) {
-            // abi_serializer::to_variant(*next,
-            //                            pretty_output,
-            //                            [](account_name n) { return optional<abi_serializer>(); },
-            //                            deadline);
+            abi_serializer::to_variant(*next,
+                                       pretty_output,
+                                       [](account_name n) { return optional<abi_serializer>(); },
+                                       deadline);
             // const auto block_id = next->id();
             // const uint32_t ref_block_prefix = block_id._hash[1];
             for (const transaction_receipt &receipt : next->transactions)
@@ -109,31 +126,63 @@ void blocklog::read_log()
                         const packed_transaction &pt = receipt.trx.get<packed_transaction>();
                         const auto &raw = pt.get_raw_transaction();
                         const auto &trx = fc::raw::unpack<transaction>(raw);
-                        bool filter = false;
+                        bool isEos = false;
+                        bool isProchaintech = false;
                         vector<fc::variant> action_variants;
                         for (const action &act : trx.actions)
                         {
+                              if (act.account == N(eosio) && act.name == N(setabi))
+                              {
+                                    eosio::chain::setabi setabi_act = act.data_as<eosio::chain::setabi>();
+                                    if (setabi_act.account == N(eosio.token))
+                                    {
+                                          std::cout << "success get eosio.token set abi" << std::endl;
+                                          eosio_token_abi_def = fc::raw::unpack<abi_def>( setabi_act.abi );
+                                    }
+                                    if (setabi_act.account == N(prochaintech)) 
+                                    {
+                                          std::cout << "success get prochaintech set abi" << std::endl;
+                                          prochaintech_abi_def = fc::raw::unpack<abi_def>(setabi_act.abi);
+                                    }
+                              }
                               if (act.account == N(eosio.token))
                               {
-                                    filter = true;
+                                    isEos = true;
                                     fc::variant action_variant;
                                     abi_serializer::to_variant(act, action_variant, [&](account_name n) {
                                           optional<abi_serializer> result = abi_serializer(eosio_token_abi_def, deadline);
                                           return result;
-                                    },
-                                                               deadline);
+                                    }, deadline);
+                                    action_variants.push_back(action_variant);
+                              }
+                              if (act.account == N(prochaintech))
+                              {
+                                    isProchaintech = true;
+                                    fc::variant action_variant;
+                                    abi_serializer::to_variant(act, action_variant, [&](account_name n) {
+                                          optional<abi_serializer> result = abi_serializer(eosio_token_abi_def, deadline);
+                                          return result;
+                                    }, deadline);
                                     action_variants.push_back(action_variant);
                               }
                         }
-                        if (filter)
+                        if (isEos)
                         {
                               fc::mutable_variant_object unpacked_transaction_variant(trx);
                               unpacked_transaction_variant.set("id", trx.id().str());
                               unpacked_transaction_variant.set("actions", action_variants);
-                              *out << fc::json::to_pretty_string(unpacked_transaction_variant) << "\n";
+                              *eos_out << fc::json::to_pretty_string(unpacked_transaction_variant) << "\n";
+                        }
+                        if (isProchaintech)
+                        {
+                              fc::mutable_variant_object unpacked_transaction_variant(trx);
+                              unpacked_transaction_variant.set("id", trx.id().str());
+                              unpacked_transaction_variant.set("actions", action_variants);
+                              *prochaintech_out << fc::json::to_pretty_string(unpacked_transaction_variant) << "\n";
                         }
                   }
             }
+
             // const auto enhanced_object = fc::mutable_variant_object
             //            ("block_num",next->block_num())
             //            ("id", block_id)
@@ -148,8 +197,6 @@ void blocklog::read_log()
       bool contains_obj = false;
       while ((block_num <= last_block) && (next = block_logger.read_block_by_num(block_num)))
       {
-            if (as_json_array && contains_obj)
-                  *out << ",";
             print_block(next);
             ++block_num;
             if (block_num % 1000 == 0)
@@ -158,20 +205,11 @@ void blocklog::read_log()
             }
             contains_obj = true;
       }
-      if (as_json_array)
-            *out << "]";
 }
 
 void blocklog::set_program_options(options_description &cli)
 {
-      cli.add_options()("blocks-dir", bpo::value<bfs::path>()->default_value("blocks"), "the location of the blocks directory (absolute path or relative to the current directory)")
-      ("output-file,o", bpo::value<bfs::path>(), "the file to write the block log output to (absolute or relative path).  If not specified then output is to stdout.")
-      ("eosio-abi-path", bpo::value<std::string>(), "eosio-abi-path")
-      ("first", bpo::value<uint32_t>(&first_block)->default_value(1), "the first block number to log")
-      ("last", bpo::value<uint32_t>(&last_block)->default_value(std::numeric_limits<uint32_t>::max()), "the last block number (inclusive) to log")
-      ("no-pretty-print", bpo::bool_switch(&no_pretty_print)->default_value(false), "Do not pretty print the output.  Useful if piping to jq to improve performance.")
-      ("as-json-array", bpo::bool_switch(&as_json_array)->default_value(false), "Print out json blocks wrapped in json array (otherwise the output is free-standing json objects).")
-      ("help", "Print this help message and exit.");
+      cli.add_options()("blocks-dir", bpo::value<bfs::path>()->default_value("blocks"), "the location of the blocks directory (absolute path or relative to the current directory)")("prochaintech-output-file", bpo::value<bfs::path>(), "the file to write the block log output to (absolute or relative path).  If not specified then output is to stdout.")("eos-output-file", bpo::value<bfs::path>(), "the file to write the block log output to (absolute or relative path).  If not specified then output is to stdout.")("eosio-abi-path", bpo::value<std::string>(), "eosio-abi-path")("first", bpo::value<uint32_t>(&first_block)->default_value(1), "the first block number to log")("last", bpo::value<uint32_t>(&last_block)->default_value(std::numeric_limits<uint32_t>::max()), "the last block number (inclusive) to log")("no-pretty-print", bpo::bool_switch(&no_pretty_print)->default_value(false), "Do not pretty print the output.  Useful if piping to jq to improve performance.")("as-json-array", bpo::bool_switch(&as_json_array)->default_value(false), "Print out json blocks wrapped in json array (otherwise the output is free-standing json objects).")("help", "Print this help message and exit.");
 }
 
 void blocklog::initialize(const variables_map &options)
@@ -184,15 +222,23 @@ void blocklog::initialize(const variables_map &options)
             else
                   blocks_dir = bld;
 
-            if (options.count("output-file"))
+            if (options.count("eos-output-file"))
             {
-                  bld = options.at("output-file").as<bfs::path>();
+                  bld = options.at("eos-output-file").as<bfs::path>();
                   if (bld.is_relative())
-                        output_file = bfs::current_path() / bld;
+                        eos_output_file = bfs::current_path() / bld;
                   else
-                        output_file = bld;
+                        eos_output_file = bld;
             }
-            eosio_token_abi_def = fc::json::from_file(options.at("eosio-abi-path").as<std::string>()).as<abi_def>();
+            if (options.count("prochaintech-output-file"))
+            {
+                  bld = options.at("prochaintech-output-file").as<bfs::path>();
+                  if (bld.is_relative())
+                        prochaintech_output_file = bfs::current_path() / bld;
+                  else
+                        prochaintech_output_file = bld;
+            }
+            eosio_abi_def = fc::json::from_file(options.at("eosio-abi-path").as<std::string>()).as<abi_def>();
       }
       FC_LOG_AND_RETHROW()
 }
