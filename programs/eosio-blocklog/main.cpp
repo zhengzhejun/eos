420be6c986eac724f473d93c841ecd6c4aaf24db
zhengzhejun <zhengzhejun@chain.pro>
2018-12-28 12:09:55 +0800

    fix

--- programs/eosio-blocklog/main.cpp
+++ programs/eosio-blocklog/main.cpp
@@ -113,10 +113,10 @@ void blocklog::read_log()
       fc::variant pretty_output;
       const fc::microseconds deadline = fc::seconds(10);
       auto print_block = [&](signed_block_ptr &next) {
-            abi_serializer::to_variant(*next,
-                                       pretty_output,
-                                       [](account_name n) { return optional<abi_serializer>(); },
-                                       deadline);
+            // abi_serializer::to_variant(*next,
+            //                            pretty_output,
+            //                            [](account_name n) { return optional<abi_serializer>(); },
+            //                            deadline);
             // const auto block_id = next->id();
             // const uint32_t ref_block_prefix = block_id._hash[1];
             for (const transaction_receipt &receipt : next->transactions)
